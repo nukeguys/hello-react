c167c79ce93c5d68f6c2f1347cfc4cf54b383635
nukeguys <nukeguys0@gmail.com>
2024-10-27 20:11:34 +0900

    4.Use js in jsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,15 @@
+function getImgSrc() {
+  return "https://i.pravatar.cc/150?img=10";
+}
+
 function TodoList() {
+  const name = "Hedy Lamarr";
+
   return (
     <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img
-        src="https://i.pravatar.cc/150?img=10"
-        alt="Hedy Lamarr"
-        className="photo"
-      />
-      <ul>
+      <h1>{name}'s Todos</h1>
+      <img src={getImgSrc()} alt={name} className="photo" />
+      <ul style={{ backgroundColor: "red" }}>
         <li>Invent new traffic lights</li>
         <li>Rehearse a movie scene</li>
         <li>Improve the spectrum technology</li>
